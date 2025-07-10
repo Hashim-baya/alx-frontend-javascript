a97bdf7ab18f2df3e0311276bb0105725d587b97
hashim-baya <bayahashim40@gmail.com>
2025-07-10 10:28:23 +0300

    Completed task 2

--- 0x04-TypeScript/task_1/dist/js/main.js
+++ 0x04-TypeScript/task_1/dist/js/main.js
@@ -6,4 +6,12 @@ var teacher3 = {
     contract: false,
 };
 console.log(teacher3);
+var director1 = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
 //# sourceMappingURL=main.js.map
