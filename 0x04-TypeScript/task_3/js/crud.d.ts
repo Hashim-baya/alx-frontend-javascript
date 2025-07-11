ea06430f444a882d462907d1f86c888d35f36c44
hashim-baya <bayahashim40@gmail.com>
2025-07-11 11:11:55 +0300

    Completed task 8

---
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -0,0 +1,13 @@
+import { RowID, RowElement } from "./interface.js"
+
+export function insertRow(row1: RowElement): RowID {
+    throw new Error("Function not implemented.");
+}
+export function updateRow(newRowID: RowID, updatedRow: RowElement) {
+    throw new Error("Function not implemented.");
+}
+
+export function deleteRow(newRowID: RowID) {
+    throw new Error("Function not implemented.");
+}
+
